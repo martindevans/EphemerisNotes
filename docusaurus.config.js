853eebfc177064c952f574dac8cf9a2ae96a4eb3
Martin Evans <martindevans@gmail.com>
2022-11-30 00:51:05 +0000

    Fixed base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Ephemeris: Space Combat Simulator',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/EphemerisNotes/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
